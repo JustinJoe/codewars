0093c30158eb777be256c7890e945db1534f7d24
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-06-22 04:58:44 +0000

    Categorises new members into senior and open

---
+++ 7 kyu/categoriseNewMember.js
@@ -0,0 +1,43 @@
+/* 
+The Western Suburbs Croquet Club has two categories of membership, Senior and Open. They would like your help with an application form that will tell prospective members which category they will be placed.
+
+To be a senior, a member must be at least 55 years old and have a handicap greater than 7. In this croquet club, handicaps range from -2 to +26; the better the player the lower the handicap.
+
+Input
+Input will consist of a list of pairs. Each pair contains information for a single potential member. Information consists of an integer for the person's age and an integer for the person's handicap.
+
+Output
+Output will consist of a list of string values (in Haskell and C: Open or Senior) stating whether the respective member is to be placed in the senior or open category.
+
+*/
+
+//P: takes in a 2-d array, made up of an array pair. The first being the age and the second, the player's handicap
+//R: Returns an array of strings - 
+//E:
+//Example
+//input =  [[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]
+//output = ["Open", "Open", "Senior", "Open", "Open", "Senior"]
+//P
+function openOrSenior(data){
+    //check age and handicap
+    //Senior: Age >= 55 && handicap > 7. Else Open
+    //Return array of category
+    let category = []
+    //First Solution
+    // for (let i = 0; i < data.length; i++){
+    //     if (data[i][0] >= 55 && data[i][1] > 7){
+    //         category.push('Senior')
+    //     } else category.push('Open')
+    // }
+    //Refactor 1
+    // data.forEach(element => {
+    //    if (element[0] >= 55 && element[1] > 7){
+    //     category.push('Senior')
+    //    } else category.push('Open')
+    // });
+    // return category
+    //Refactor 2
+    return data.map(([age, handicap]) => (age >= 55 && handicap > 7) ? 'Senior' : 'Open')
+}
+
+console.log(openOrSenior([[18, 20], [45, 2], [61, 12], [37, 6], [21, 21], [78, 9]]))
