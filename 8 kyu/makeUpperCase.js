c48f1c1db049c3d1d243e6aa896ec36f5786271c
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-06-28 23:45:57 +0000

    Capitalises an input string

---
+++ 8 kyu/makeUpperCase.js
@@ -0,0 +1,13 @@
+/* 
+Write a function which converts the input string to uppercase.
+*/
+//P: Takes a string as the parameter
+//R: Returns the string capitalised
+//E: 
+//makeUpperCase('hello') //HELLO
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+console.log(makeUpperCase('hello'))
