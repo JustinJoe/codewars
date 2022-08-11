086107fc778779487d472cb23539eca3f636dc7c
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-08-11 23:50:27 +0000

    Validates PIN codes - refactored

--- 7kyu/validatePinCode.js
+++ 7kyu/validatePinCode.js
@@ -8,20 +8,20 @@ If the function is passed a valid PIN string, return true, else return false.
 //P: Takes in a string as a parameter
 //R: Returns true if the parameter is exactly 4 or 6 numbers only
 //E: "1234"   -->  true; "12345"  -->  false; "a234"   -->  false
-//P: 
+//P:
 function isValidPIN(pin) {
-    //Get regex to validate pin
-    //Test pin with regex
+  //Get regex to validate pin
+  //Test pin with regex
 
-    let regex = /^\d{4}$|^\d{6}$/;
+  let regex = /^(\d{4}$|^\d{6})$/;
 
-    return regex.test(pin);
+  return regex.test(pin);
 }
-console.log(!isNaN(' '))
-console.log(isValidPIN('1234 '))
-console.log(isValidPIN('1234'))
-console.log(isValidPIN('12345'))
-console.log(isValidPIN('a234'))
-console.log(isValidPIN('0000'))
-console.log(isValidPIN('-1234'))
-console.log(isValidPIN('-1.234'))
+
+console.log(isValidPIN("1234 "));
+console.log(isValidPIN("1234"));
+console.log(isValidPIN("12345"));
+console.log(isValidPIN("a234"));
+console.log(isValidPIN("0000"));
+console.log(isValidPIN("-1234"));
+console.log(isValidPIN("-1.234"));
