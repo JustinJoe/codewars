d450018addd6d58225750b299afcd4103c572081
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-08-21 21:52:45 +0000

    Decides whether to set alarm if vacation is false and employed is true

---
+++ 8kyu/setAlarm.js
@@ -0,0 +1,28 @@
+/* 
+Write a function named setAlarm which receives two parameters. The first parameter, employed, is true 
+whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+The function should return true if you are employed and not on vacation (because these are the 
+circumstances under which you need to set an alarm). It should return false otherwise. 
+
+*/
+
+//P: Takes two parameters - employed and vacation
+//R: Returns a boolean
+//E:
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+//P:
+
+function setAlarm(employed, vacation) {
+  //if employed is true, and vacation is false, return true
+  //else return false
+  return employed === true && vacation === false ? true : false;
+}
+
+console.log(setAlarm(true, true));
+console.log(setAlarm(false, true));
+console.log(setAlarm(false, false));
+console.log(setAlarm(true, false));
