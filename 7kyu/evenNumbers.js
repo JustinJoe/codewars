fba0d974839ee0a38156161b93b0bc8f2537fa87
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-10-04 23:34:58 +0000

    Gets an array of the last even numbers  in the same order

---
+++ 7kyu/evenNumbers.js
@@ -0,0 +1,30 @@
+/* 
+Given an array of numbers, return a new array of length number containing the last even 
+numbers from the original array (in the same order). The original array will be not empty 
+and will contain at least "number" even numbers.
+
+*/
+// P: Takes in an array and an integer
+// R: Returns an array of even numbers with length of integer
+// E:
+// ([1, 2, 3, 4, 5, 6, 7, 8, 9], 3) => [4, 6, 8]
+// ([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2) => [-8, 26]
+// ([6, -25, 3, 7, 5, 5, 7, -3, 23], 1) => [6]
+// P:
+function evenNumbers(array, number) {
+  // filter out even numbers and index + 1 <= number
+
+  array = array.filter((num) => num % 2 === 0);
+  let arr = [];
+
+  for (let i = array.length - 1; number > 0; i--) {
+    arr.unshift(array[i]);
+    number--;
+  }
+
+  return arr;
+}
+
+console.log(evenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9], 3));
+console.log(evenNumbers([-22, 5, 3, 11, 26, -6, -7, -8, -9, -8, 26], 2));
+console.log(evenNumbers([6, -25, 3, 7, 5, 5, 7, -3, 23], 1));
