707487a12dcde9cb4a63844fe66f58ab17fe1525
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-10-16 23:29:35 +0000

    squares a number

---
+++ 8kyu/square.js
@@ -0,0 +1,14 @@
+/* 
+Now you have to write a function that takes an argument and returns the square of it.
+*/
+
+// P: Takes in an integer
+// R: Returns an integer
+// E:
+// square(3) --> 9
+// P:
+function square(num) {
+  return Math.pow(num, 2);
+}
+
+console.log(square(3));
