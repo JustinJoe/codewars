2122a445c59bdf77726618b50ab6376e2da3d305
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-10-26 21:13:59 +0000

    Checks if every element in an array is true for a given function

---
+++ 8kyu/allTrue.js
@@ -0,0 +1,30 @@
+/* 
+Create a method all which takes two params:
+
+a sequence
+a function (function pointer in C)
+and returns true if the function in the params returns true for every element in the 
+sequence. Otherwise, it should return false. If the sequence is empty, it should return 
+true, since technically nothing failed the test.
+
+*/
+// P: Takes in an array and a function
+// R: Returns a boolean
+// E:
+// allTrue([1, 2, 3, 4, 5], function(v){return v > 9}) -> false
+// allTrue([1, 2, 3, 4, 5], function(v){return v < 9}) -> True
+// P:
+function allTrue(arr, fun) {
+  return arr.every((num) => fun(num));
+}
+
+console.log(
+  allTrue([1, 2, 3, 4, 5], function (v) {
+    return v > 9;
+  })
+);
+console.log(
+  allTrue([1, 2, 3, 4, 5], function (v) {
+    return v < 9;
+  })
+);
