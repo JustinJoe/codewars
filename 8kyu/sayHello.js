685507aec87d4bd6df7867d5c3553ec6550bb21b
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-11-11 22:48:44 +0000

    Welcomes a person to a city

---
+++ 8kyu/sayHello.js
@@ -0,0 +1,24 @@
+/* 
+Create a method sayHello/say_hello/SayHello that takes as input a name, city, and 
+state to welcome a person. Note that name will be an array consisting of one or 
+more values that should be joined together with one space between each, and the 
+length of the name array in test cases will vary.
+*/
+// P: Takes in an array and two strings
+// R: Returns a string
+// E:
+// sayHello(['John', 'Smith'], 'Phoenix', 'Arizona') --> 
+// 'Hello, John Smith! Welcome to Phoenix, Arizona!'
+// sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois') --> 
+// 'Hello, Franklin Delano Roosevelt! Welcome to Chicago, Illinois!'
+// sayHello(['Wallace','Russel','Osbourne'],'Albany','New York') --> 
+// 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
+// P:
+function sayHello(name, city, state) {
+    let fullName = name.join(" ");
+    return `Hello, ${fullName}! Welcome to ${city}, ${state}!`
+}
+
+console.log(sayHello(['John', 'Smith'], 'Phoenix', 'Arizona'))
+console.log(sayHello(['Franklin','Delano','Roosevelt'], 'Chicago', 'Illinois'))
+console.log(sayHello(['Wallace','Russel','Osbourne'],'Albany','New York'))
