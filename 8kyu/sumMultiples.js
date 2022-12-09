15124070daa82768bcd0d349afaf46e31a6749c8
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2022-12-09 19:02:21 +0000

    Sums up all multiples of num1 below num2

---
+++ 8kyu/sumMultiples.js
@@ -0,0 +1,33 @@
+/* 
+Find the sum of all multiples of n below m
+
+Keep in Mind
+n and m are natural numbers (positive integers)
+m is excluded from the multiples
+Examples
+sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+sumMul(4, -7)  ==> "INVALID"
+*/
+// P: Takes in two integers
+// R: Returns an integer
+// E:
+// P:
+function sumMul(num1, num2) {
+  let sum = 0;
+
+  if (num1 >= num2) {
+    return "INVALID";
+  } else {
+    for (let i = num1; i < num2; i += num1) {
+      sum += i;
+    }
+  }
+  return sum;
+}
+
+console.log(sumMul(2, 9));
+console.log(sumMul(3, 13));
+console.log(sumMul(4, 123));
+console.log(sumMul(4, -7));
