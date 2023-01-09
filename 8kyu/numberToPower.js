01fe166ed12959bb9eb7ee375c4574b709b7cbe1
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2023-01-09 22:52:10 +0000

    Evaluates a number to a certain power

---
+++ 8kyu/numberToPower.js
@@ -0,0 +1,31 @@
+/* 
+The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+// P: Takes in two integers
+// R: Returns an integer
+// E:
+// numberToPower(3,2)   --> 9 ( = 3 * 3 )
+// numberToPower(2,3)   --> 8 ( = 2 * 2 * 2 )
+// numberToPower(10,6)  --> 1000000
+// numberToPower(4, 2)  --> 16;
+// numberToPower(10, 4) --> 10000;
+// numberToPower(10, 0) --> 1;
+// P:
+function numberToPower(number, power) {
+  let product = 1;
+
+  for (let i = 0; i < power; i++) {
+    product *= number;
+  }
+
+  return product;
+}
+
+console.log(numberToPower(3, 2));
+console.log(numberToPower(2, 3));
+console.log(numberToPower(10, 6));
+console.log(numberToPower(4, 2));
+console.log(numberToPower(10, 4));
+console.log(numberToPower(10, 0));
