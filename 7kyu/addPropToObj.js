f268b2d17de20d9b8210bc0582701bb605e61ea1
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2023-01-29 21:21:59 +0000

    Adds a property to objects in an array

---
+++ 7kyu/addPropToObj.js
@@ -0,0 +1,47 @@
+/* 
+Your task is to add a new property usersAnswer to every object in the array questions. 
+The value of usersAnswer should be set to null. The solution should work for array of any 
+length.
+
+*/
+// P: Takes in an array
+// R: Returns an array
+// E:
+// let questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+//     corAnswer: 0
+// }];
+// After adding the property the result should be:
+// let questions = [{
+//     question: "What's the currency of the USA?",
+//     choices: ["US dollar", "Ruble", "Horses", "Gold"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }, {
+//     question: "Where was the American Declaration of Independence signed?",
+//     choices: ["Philadelphia", "At the bottom", "Frankie's pub", "China"],
+//     corAnswer: 0,
+//     usersAnswer: null
+// }];
+// P:
+
+let questions = [
+  {
+    question: "What's the currency of the USA?",
+    choices: ["US dollar", "Ruble", "Horses", "Gold"],
+    corAnswer: 0,
+  },
+  {
+    question: "Where was the American Declaration of Independence signed?",
+    choices: ["Philadelphia", "At the bottom", "Frankie's Pub", "China"],
+    corAnswer: 0,
+  },
+];
+questions.map((question) => (question.usersAnswer = null));
+
+console.log(questions);
