14e0f3390b7e755b1b49af42e5e428b863d3cb2e
JustinJoe <63883279+JustinJoe@users.noreply.github.com>
2023-01-29 21:45:15 +0000

    Splits a string into different strings of equal size

---
+++ 7kyu/splitInParts.js
@@ -0,0 +1,24 @@
+/* 
+The aim of this kata is to split a given string into different strings of equal size 
+(note size of strings is passed to the method)
+*/
+// P: Takes in a string
+// R: Returns a string
+// E:
+// splitInParts("supercalifragilisticexpialidocious", 3) --> "sup erc ali fra gil ist ice xpi ali doc iou s";
+// splitInParts("HelloKata", 1) --> "H e l l o K a t a";
+// splitInParts("HelloKata", 9) --> "HelloKata";
+// P:
+function splitInParts(string, num) {
+  let arr = [];
+
+  for (let i = 0; i < string.length; i += num) {
+    arr.push(str.slice(i, (i += num)));
+  }
+
+  return arr;
+}
+
+console.log(splitInParts("supercalifragilisticexpialidocious", 3));
+console.log(splitInParts("HelloKata", 1));
+console.log(splitInParts("HelloKata", 9));
